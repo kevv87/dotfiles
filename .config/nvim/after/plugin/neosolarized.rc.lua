4a3ce897d82b566506d3595374ed36876bcc83bf
Kevin Zeledón <magneticok@gmail.com>
2023-03-18 08:54:05 -0600

    Updating nvim

---
+++ .config/nvim/after/plugin/neosolarized.rc.lua
@@ -0,0 +1,6 @@
+local status, n = pcall(require, 'neosolarized')
+if (not status) then return end
+
+n.setup({
+  commit_italics = true
+})
---
+++ .config/nvim/lua/windows.lua
@@ -0,0 +1,1 @@
+vim.opt.clibpoard:preprend { 'unnamed', 'unnamedplus' }
